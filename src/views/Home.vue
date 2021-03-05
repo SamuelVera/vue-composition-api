f1f92d9d1e60b481cfd22e1375201062ef6f3f17
Samuel Vera <samuelvera1999@gmail.com>
2021-03-05 14:01:50 -0400

    Refs for reactivity lesson

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <!--To use a ref from the Composition API use the variable name without binding-->
-    <p ref="p">Subject: {{ name }}</p>
+    <!--To use a ref from the Composition API use the variable name without binding Ex: ref="p"-->
+    <!--No need to use ref.value inside the template-->
+    <p>Subject: {{ name }}</p>
     <p>Age: {{ age }}</p>
+
+    <br />
     <button @click="handleClick">Click Me!</button>
+
+    <br />
+    <button @click="age++">Add age</button>
+
+    <br />
+    <button @click="age--">Reduce age</button>
+
+    <br />
+    <!--2-way data binding with ref values-->
+    <input type="text" v-model="name" />
   </div>
 </template>
 
@@ -16,28 +29,40 @@ export default {
   //Composition API function
   setup() {
     //Will run before all the lifecycle hooks (first to execute to setup all)
+
     //JS can be written
-    let name = "Jvnko";
-    let age = 20;
+    ////This values aren't reactive
+    //let name = "Jvnko";
+    //let age = 20;
+
+    //Refs can be used to apply reactivity to values built in setup
+    //This values are reactive
+    const name = ref("Jvnko");
+    const age = ref(21);
 
     //Refs creation (to use normal JS with this)
-    const p = ref(null); //Initially its null
+    //const p = ref(null); //Initially its null
 
     const handleClick = () => {
-      if (p.value !== null) {
-        //Only allow when the ref is attached to the DOM element
-        p.value.classList.add("test");
-        p.value.textContent = "Good morning!";
-      }
+      //Wait for DOM refs to be attached
+
+      // if (p.value !== null) {
+      //   //Only allow when the ref is attached to the DOM element
+      //   p.value.classList.add("test");
+      //   p.value.textContent = "Good morning!";
+      // }
+
+      //Because refs are reactive this will update the template values
+      name.value = "Furuta";
+      age.value = 20;
     };
 
     //Return data to use in the template
-    //This values aren't reactive
     return {
       name, //This can be used in the template
       age, //This can be used in the template
       handleClick, //This can be used in the template
-      p, //Return the ref to assing it to a DOM elment
+      //p, //Return the ref to assing it to a DOM elment
     };
   },
 };
