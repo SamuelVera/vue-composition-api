899502905f34794bd95eec25fb27632cb92137a2
Samuel Vera <samuelvera1999@gmail.com>
2021-03-05 13:45:05 -0400

    Composition API basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>Subject: {{ name }}</p>
+    <p>Age: {{ age }}</p>
+    <button @click="handleClick">Click Me!</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  //Composition API function
+  setup() {
+    //Will run before all the lifecycle hooks (first to execute to setup all)
+    //JS can be written
+    let name = "Jvnko";
+    let age = 20;
+
+    const handleClick = () => {
+      console.log("CLICKED ME!");
+    };
+
+    //Return data to use in the template
+    //This values aren't reactive
+    return {
+      name, //This can be used in the template
+      age, //This can be used in the template
+      handleClick, //This can be used in the template
+    };
+  },
+};
 </script>
